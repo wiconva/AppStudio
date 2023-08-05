82ded8f0c9228cbb89d2c4c58aa5b51b0375a73e
wiconva <wiconva@gmail.com>
2023-08-05 15:59:59 -0500

    Adding header title.

--- src/main/java/appStudio/Client/view/javaScript/header.js
+++ src/main/java/appStudio/Client/view/javaScript/header.js
@@ -3,6 +3,7 @@ document.write("<div>"+
                     "<a href=\"#\" id='menucliente'><button onmouseover='submenuCliente()'>Clientes</button></a>"+
                 "</nav>"+
             "</div>"+
+            "<div id='headerTitle'>AppStudio</div>"+
     "<div id='subcliente' hidden= 'hidden' onmouseleave='submenuCliente()'>"+
         "<a href='CrearCliente.html' ><button >Crear Cliente</button></a><br>"+
         "<a href='ConsultarClientes.html'><button>Consultar Clientes</button></a><br>"+
